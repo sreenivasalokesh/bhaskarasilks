43d59b869356d8430c90cbc33fcbec9e223a67d3
Lokesh <slokesh@bmc.com>
2025-07-21 09:38:49 +0530

    tab responsiveness added

--- src/component/home/HeroImage.js
+++ src/component/home/HeroImage.js
@@ -12,11 +12,8 @@ const HeroImage = () => {
       <div className='rightPanel'>
         <div className='card'>
           <div className='text'>
-            <span className='first'><span className='firstletter'>C</span> <span className='remainigFirst'>rafted</span> by artisans. &nbsp;</span>
-            <span className='second'><span className='firstletter'>C</span> <span className='remainigFirst'>ommitted</span> to quality. &nbsp;</span>
-            <br/>
-            <span className='third'><span className='special'><span className='firstletter'>C</span> <span className='remainigFirst'>haracter </span>&nbsp;that builds trust.</span></span>
-          </div>
+           <span className='firstletter'>E</span> <span className='remainigFirst'>xpertly crafted, with a commitment to quality and a character that endures.</span>
+           </div>
           <span className='companycontainer'>
             <span className='subtextcontainer'><span className='subtext'>Bhaskara Silks</span></span>
             <span className='sincecontainer'><span className='since'>Trusted Saree Manufacturers Since 1975 &nbsp;&nbsp;&nbsp;</span></span>
@@ -33,3 +30,13 @@ const HeroImage = () => {
 
 
 export default HeroImage;
+
+// Crafted by skilled artisans, committed to quality, and built on character that inspires trust.
+//Expertly crafted with care and tradition, bound by a promise of unmatched quality and a character that earns lasting trust
+
+//  <span className='first'><span className='firstletter'>C</span> <span className='remainigFirst'>rafted</span> by artisans. &nbsp;</span>
+//             <br/>
+//             <span className='second'><span className='firstletter'>C</span> <span className='remainigFirst'>ommitted</span> to quality. &nbsp;</span>
+//             <br/>
+//             <span className='third'><span className='special'><span className='firstletter'>C</span> <span className='remainigFirst'>haracter </span>&nbsp;that builds trust.</span></span>
+         
